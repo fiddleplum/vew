c46cd1ccffb31b213946daa94a0e969d1beb28c2
Stephen Hurley <stephenhurley@gmail.com>
2017-09-06 17:27:14 -0700

    Added ability to use external components.

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 rm -rf out
 mkdir out
-emcc src/*.cpp --bind -std=c++11 -O3 -s USE_SDL=2 -s WASM=1 -o out/vew.js
+
+BUILD_COMPONENTS_FILE="build_components.txt"
+BUILD_COMPONENTS=''
+
+if [ -f $BUILD_COMPONENTS_FILE ]; then
+	echo "Found $BUILD_COMPONENTS_FILE"
+	while IFS='' read -r line || [[ -n "$LINE" ]]; do
+		echo "  Adding $LINE"
+		BUILD_COMPONENTS="$BUILD_COMPONENTS $LINE"
+	done < $BUILD_COMPONENTS_FILE
+fi
+
+echo "Building VEW"
+emcc src/*.cpp $BUILD_COMPONENTS --bind -std=c++11 -O3 -s USE_SDL=2 -s WASM=1 -o out/vew.js
